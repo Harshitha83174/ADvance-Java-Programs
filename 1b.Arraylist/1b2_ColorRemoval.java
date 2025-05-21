de5ead3daac9473e507dd13f7afcd466b19954d0
Harshitha M <153185218+Harshitha83174@users.noreply.github.com>
2025-05-21 20:36:03 +0530

    Create 1b2_ColorRemoval.java

---
+++ 1b.Arraylist/1b2_ColorRemoval.java
@@ -0,0 +1,40 @@
+/*1b2.Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
+package Lab1;
+import java.util.ArrayList;
+
+public class 1b2_ColorRemoval {
+    public static void main(String[] args) {
+        // Create an ArrayList of colors
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Add colors to the list
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+        colors.add("Black");
+
+        // Display original list
+        System.out.println("Original color list: " + colors);
+
+        // 1. Remove the 2nd element (index 1)
+        if (colors.size() > 1) {
+            String removedColor = colors.remove(1);  // index 1 = 2nd element
+            System.out.println("Removed 2nd element: " + removedColor);
+        } else {
+            System.out.println("List has less than 2 elements. Cannot remove 2nd element.");
+        }
+
+        // 2. Remove the color "Blue"
+        boolean blueRemoved = colors.remove("Blue");
+        if (blueRemoved) {
+            System.out.println("Removed color: Blue");
+        } else {
+            System.out.println("Color 'Blue' was not found in the list.");
+        }
+
+        // Display final list
+        System.out.println("Updated color list: " + colors);
+    }
+}
